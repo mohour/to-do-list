fe28c39dc78639f8c247371989e583867f5205d0
mohour <moh.our.48@gmail.com>
2025-06-20 11:39:57 +0100

    Refactor environment configuration and update database connection settings; add .gitignore for sensitive files

--- Server/db.js
+++ Server/db.js
@@ -1,12 +1,13 @@
-const {Pool}= require('pg');
+const env = require("dotenv");
+env.config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user:"postgres",
-    password:"Simolool1997",
-    host:"localhost",
-    port:5432,
-    database:"todolist"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
-
